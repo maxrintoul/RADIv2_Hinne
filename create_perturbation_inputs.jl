6699b5f4090c8e5b12fc159347550de2321d4717
maxrintoul <61886918+maxrintoul@users.noreply.github.com>
2026-05-04 16:44:59 +1000

    Start making file to read in model outputs

---
+++ create_perturbation_inputs.jl
@@ -0,0 +1,12 @@
+# Load model output
+using MAT
+
+model_output = matread("/Users/maxrintoul/repos/RADIv2_09_03_2026/gadi_output/time_cost_tests/sols_all_20260430_055053_725_job167413397-gadi-pbs_pid374883_7a87cf90-2ab6-4df3-a616-e86a2cd4591e.mat")
+
+
+
+# Get model inputs for each run 
+
+# Get final model output for each run
+
+# Get model inputs for each run 
